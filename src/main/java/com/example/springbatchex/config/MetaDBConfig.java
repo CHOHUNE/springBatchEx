0402e8a65db56f4f249c76bcda484dba6f0b00d1
CHOHUNE <yuureru@gmail.com>
2024-07-29 16:06:53 +0900

    다중 데이터 베이스 설정

---
+++ src/main/java/com/example/springbatchex/config/MetaDBConfig.java
@@ -0,0 +1,30 @@
+package com.example.springbatchex.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MetaDBConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.meta")// ApplicationProperties 에 있는 변수 설정값을 불러오는 애너테이션
+    public DataSource metaDBSource() {
+
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager metaTransactionManager() {
+
+        return new DataSourceTransactionManager(metaDBSource());
+    }
+}
---
+++ src/test/java/com/example/springbatchex/SpringBatchExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
