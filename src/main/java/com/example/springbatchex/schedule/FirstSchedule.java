e14981bb6f57fca080cbfc1664e0553878cf8592
CHOHUNE <yuureru@gmail.com>
2024-07-29 17:48:08 +0900

    스케쥴링 방식의 배치

---
+++ src/main/java/com/example/springbatchex/schedule/FirstSchedule.java
@@ -0,0 +1,39 @@
+package com.example.springbatchex.schedule;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Configuration
+@RequiredArgsConstructor
+public class FirstSchedule {
+
+
+    private final JobLauncher jobLauncher;
+    private final JobRegistry jobRegistry;
+
+    @Scheduled(cron = "10 * * * * *", zone = "Asia/Seoul") // 매분 10초마다 실행, 크론 작성 방법은 검색으로 확인
+    public void runFirstJob() throws Exception {
+
+        System.out.println("first schedule start");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss");
+        String date = dateFormat.format(new Date());
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date", date)
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("firstJob"), jobParameters);
+
+    }
+
+
+}
